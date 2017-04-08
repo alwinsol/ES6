9eaddfaf3c3826752e26e1257cf354ebde0aacf2
Alwin Solanky <alwinsol@yahoo.co.uk>
2017-04-08 16:29:05 +0100

    adding first files

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: ['./app/index.js'],
+  output: {
+    // path: './build',
+    // filename: 'bundle.js'
+
+    //path: __dirname + '/build'
+
+    path: __dirname + '/dist/js'
+  }
+}
